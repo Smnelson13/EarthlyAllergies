6460d8f9c39e9840c348d845a3e61a31f88b5a13
shane nelson <smnelson13@live.com>
2017-04-05 21:46:29 -0400

    removed api key

--- EarthlyAllergies/APIController.swift
+++ EarthlyAllergies/APIController.swift
@@ -11,9 +11,8 @@ import Foundation
 struct API
 {
   
-  static let APIKey = "c3d8d4c9641dfcf8bda6a087e1f55f8b"
-  static let BaseURL = URL(string: "https://api.darksky.net/forecast/c3d8d4c9641dfcf8bda6a087e1f55f8b/37.8267,-122.4233")!
-  static var AuthenticatedBaseURL: URL
+  static let APIKey =
+  static let BaseURL =   static var AuthenticatedBaseURL: URL
   {
     return BaseURL.appendingPathComponent(APIKey)
   }
