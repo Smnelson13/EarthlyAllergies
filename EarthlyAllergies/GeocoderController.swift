49b172d1401e045cc189038766e67c362c411ff6
shane nelson <smnelson13@live.com>
2017-04-09 17:46:00 -0400

    done except a few buttons to hook up

--- EarthlyAllergies/GeocoderController.swift
+++ EarthlyAllergies/GeocoderController.swift
@@ -5,5 +5,26 @@
 //  Created by shane nelson on 4/8/17.
 //  Copyright © 2017 The Iron Yard. All rights reserved.
 //
-
+import CoreLocation
 import Foundation
+//
+//let geocoder = CLGeocoder()
+//
+//geocoder.geocodeAddressString("Lakeland, FL", completionHandler: {
+//  placemarks, error in
+//  if let geocodeError = error
+//  {
+//    print(geocodeError.localizedDescription)
+//  }
+//  else
+//  {
+//    if let placemark = placemarks?[0]
+//    {
+//      let annotation = MKPointAnnotation()
+//      annotation.coordinate = (placemark.location?.coordinate)!
+//      annotation.title = "Lakeland, FL"
+//      self.mapView.addAnnotation(annotation)
+//    }
+//  }
+//})
+//
--- EarthlyAllergies/ZipcodeViewController.swift
+++ EarthlyAllergies/ZipcodeViewController.swift
@@ -5,31 +5,93 @@
 //  Created by shane nelson on 4/9/17.
 //  Copyright © 2017 The Iron Yard. All rights reserved.
 //
-
+import CoreLocation
 import UIKit
 
-class ZipcodeViewController: UIViewController {
+protocol ZipcodeViewControllerDelegate
+{
+//  func didRecieve(_ results: [Any])
+  
+  func zipcodeViewControllerDidReceiveLocationCoordinate(coordinate: CLLocationCoordinate2D)
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class ZipcodeViewController: UIViewController, UISearchBarDelegate
+{
+  @IBOutlet weak var searchBar: UISearchBar!
+  @IBOutlet weak var cancelButton: UIButton!
+  @IBOutlet weak var searchButton: UIButton!
+  var delegate: ZipcodeViewControllerDelegate!
+  
+  
+  
+  /*
+  init(delegate: APIControllerProtocol)
+  {
+    self.delegate = delegate as! GeocoderAPIProtocol
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+   */
+ 
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+    searchBar.delegate = self
+    // todo: change searchbar text color
+  }
 
-        // Do any additional setup after loading the view.
-    }
+  override func didReceiveMemoryWarning()
+  {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String)
+  {
+    print("searchText \(searchText)")
+  }
+  
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+  {
+    performGeocode(text: searchBar.text)
+  }
+  
+  func performGeocode(text: String?)
+  {
+    guard let text = text, text != "" else
+    {
+      return
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    let geocoder = CLGeocoder()
+    geocoder.geocodeAddressString(text) { placemarks, error in
+      if let coordinate = placemarks?.first?.location?.coordinate {
+        self.delegate.zipcodeViewControllerDidReceiveLocationCoordinate(coordinate: coordinate)
+      }
     }
-    */
-
+  }
+ 
+//  func geocoderAPI()
+//  {
+//  
+//  let geocoder = CLGeocoder()
+//  
+//  geocoder.geocodeAddressString(searchBar.text!, completionHandler:
+//  {
+//  placemarks, error in
+//  if let geocodeError = error
+//  {
+//  print(geocodeError.localizedDescription)
+//  }
+//  else
+//  {
+//  if let placemark = placemarks?[0]
+//  {
+//    placemark.location?.coordinate
+//  }
+//  }
+//  })
+//  }
 }
